948662a1194183d76d93aa7649ae70d5827d6b84
黄志平 <10049642+hzp13960020433@user.noreply.gitee.com>
2022-07-01 10:34:41 +0800

    city-search

---
+++ src/components/city/CitySearch.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="city-search">
+    <input
+      class="search-input"
+      type="text"
+      placeholder="输入城市名或拼音"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '@/assets/styles/varibles.scss';
+.city-search {
+  padding: 0 0.1rem;
+  height: 0.72rem;
+  background-color: $bgColor;
+  .search-input {
+    box-sizing: border-box;
+    line-height: 0.62rem;
+    height: 0.62rem;
+    width: 100%;
+    height: 0.62rem;
+    text-align: center;
+    color: #666;
+    padding: 0 0.1rem;
+  }
+}
+</style>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,13 +1,18 @@
 <template>
-  <CityHeader></CityHeader>
+  <div class="city">
+    <CityHeader></CityHeader>
+    <CitySearch></CitySearch>
+  </div>
 </template>
 
 <script>
 import CityHeader from "@/components/city/CityHeader"
+import CitySearch from "@/components/city/CitySearch"
 export default {
   name: 'CityView',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
